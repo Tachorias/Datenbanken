4217460fc2b269bf437e49c3a9b25695c2fd1ecc
Tachorias <zachilachi@googlemail.com>
2026-04-21 10:37:28 +0200

    Tailwind und erste Tests

---
+++ src/app/test/test.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-test',
+  imports: [FormsModule],
+  templateUrl: './test.html',
+  styleUrl: './test.css',
+})
+export class Test {
+  username = "Nutzername"
+  clickbar = true
+  title = "Sexy Silvan"
+  onButtonClick() {
+    alert("Tim stinkt!");
+    this.clickbar = false
+  }
+  filme = [{name: "Star Wars 1",
+                                            datum: "21.04.2026",
+                                              time: "09:00"},
+                                        {name: "Star Wars 2",
+                                            datum: "21.04.2026",
+                                              time: "12:00"},
+                                        {name: "Star Wars 3",
+                                            datum: "21.04.2026",
+                                              time: "15:00"}]
+}
